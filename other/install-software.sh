a8b87bc2c1db924fc2fbe4bffed15c4abd6b8b62
Pedro Narciso García Revington <p.revington@gmail.com>
2013-05-30 10:32:53 +0200

    line break

--- other/install-software.sh
+++ other/install-software.sh
@@ -1,13 +1,41 @@
 apt-get update
 apt-get upgrade
-apt-get install curl gitk toilet vim vlc \
-  unrar p7zip-full tmux g++ automake \
-  libssl-dev openssh-server make clementine \
-  exuberant-ctags tidy openjdk-7-jdk firefox \
-  ufraw xsltproc maven2 openjdk-7-jre smartmontools \
-  libmozjs185-dev libicu-dev libcurl4-gnutls-dev \
-  libtool erlang-dev erlang-manpages erlang-base-hipe \
-  erlang-eunit erlang-nox erlang-xmerl erlang-inets \
-  help2man gcc zlib1g-dev libssl-dev rake help2man \
-  texinfo flex dctrl-tools libsctp-dev libxslt1-dev \
-  libcap2-bin ed gimp apache2-utils gnuplot
+apt-get install \
+gitk  \
+toilet  \
+vim  \
+vlc \
+unrar  \
+p7zip-full  \
+tmux \
+g++ \
+automake \
+libssl-dev  \
+openssh-server \
+make \
+clementine \
+exuberant-ctags  \
+tidy \
+firefox \
+ufraw \
+firefox \
+xsltproc \
+maven2 \
+openjdk-7-jdk  \
+openjdk-7-jre \
+smartmontools  \
+libmozjs185-dev  \
+libicu-dev  \
+libcurl4-gnutls-dev  \
+libtool \
+erlang-dev  \
+erlang-manpages  \
+erlang-base-hipe  \
+erlang-eunit  \
+erlang-nox  \
+erlang-xmerl  \
+erlang-inets \
+help2man zlib1g-dev rake texinfo flex dctrl-tools libsctp-dev libxslt1-dev libcap2-bin ed \
+gimp \
+apache2-utils \
+gnuplot \

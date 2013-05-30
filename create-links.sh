a90fa42ef043ffe02d6679cab6cef5873f7c55ac
Pedro Narciso García Revington <p.revington@gmail.com>
2013-05-30 09:37:29 +0200

    fixed typo "README" + ignore "other" dir

--- create-links.sh
+++ create-links.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+ignore=("create-links.sh" "README.md" "other")
+
 for file in [a-zA-Z]* ; do
-	if [[ "$file" != "create-links.sh" && "$file" != "README" ]] ; then
-		echo "Linking ~/.$file to `pwd`/$file"
+  case "${ignore[@]}" in  *$file*) continue ;; esac
+  echo "Linking ~/.$file to `pwd`/$file"
 
-		if [[ -h ~/."$file" ]] ; then
-			echo "  Removing symbolic link ~/.$file"
-			rm ~/."$file"
-		elif [[ -a ~/."$file" ]] ; then
-			echo "  ~/.$file exists. Moving it to ~/.$file.back"
-			mv ~/."$file" ~/."$file".back || echo "Couldn't move ~/.$file to ~/.$file.back!"
-		fi
+  if [[ -h ~/."$file" ]] ; then
+      echo "  Removing symbolic link ~/.$file"
+      rm ~/."$file"
+  elif [[ -a ~/."$file" ]] ; then
+      echo "  ~/.$file exists. Moving it to ~/.$file.back"
+      mv ~/."$file" ~/."$file".back || echo "Couldn't move ~/.$file to ~/.$file.back!"
+  fi
 
-		ln -s `pwd`/"$file" ~/."$file" || echo "  Couldn't link ~/.$file to `pwd`/$file!"
-	fi
+  ln -s `pwd`/"$file" ~/."$file" || echo "  Couldn't link ~/.$file to `pwd`/$file!"
 done
